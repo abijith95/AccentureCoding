4148c7029b889b033c36a9c21d5d295cff3059fa
abijith95 <nabijith95@gmail.com>
2024-10-14 20:28:04 +0530

    new commit

--- src/MoveHypens/Main.java
+++ src/MoveHypens/Main.java
@@ -2,6 +2,26 @@ package MoveHypens;
 
 import java.util.Scanner;
 
+//        The function accepts a string “str” of length ‘n’, that contains alphabets and hyphens (-). Implement the function to move all hyphens(-) in the string to the front of the given string.
+//
+//        NOTE:- Return null if str is null.
+//
+//        Example :-
+//
+//        Input:
+//        str.Move-Hyphens-to-Front
+//        Output:
+//        —MoveHyphenstoFront
+//        Explanation:-
+//
+//        The string “Move-Hyphens -to-front” has 3 hyphens (-), which are moved to the front of the string, this output is “— MoveHyphen”
+//
+//        Sample Input
+//
+//        Str: String-Compare
+//        Sample Output-
+//
+//        -StringCompare
 public class Main {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
--- src/OperationChoice/Main.java
+++ src/OperationChoice/Main.java
@@ -2,6 +2,35 @@ package OperationChoice;
 
 import java.util.Scanner;
 
+//Int OperationChoices(int c, int n, int a , int b )
+//
+//        The function accepts 3 positive integers ‘a’ , ‘b’ and ‘c ‘ as its arguments. Implement the function to return.
+//
+//        ( a+ b ) , if c=1
+//        ( a – b ) , if c=2
+//        ( a * b ) ,  if c=3
+//        (a / b) ,  if c =4
+//        Assumption : All operations will result in integer output.
+//
+//        Example:
+//
+//        Input
+//        c :1
+//        a:12
+//        b:16
+//        Output:
+//        Since ‘c’=1 , (12+16) is performed which is equal to 28 , hence 28 is returned.
+//        Sample Input
+//
+//        c : 2
+//
+//        a : 16
+//
+//        b : 20
+//
+//        Sample Output
+//
+//        -4
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
