ca42830656ddaaf3f371546c2e10f21e61bb1c54
abijith95 <nabijith95@gmail.com>
2024-10-15 15:05:39 +0530

    new commit

---
+++ src/Prime/Main.java
@@ -0,0 +1,27 @@
+package Prime;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if(n < 2)
+        {
+            System.out.println("False");
+        }else
+        {
+            for(int i=2;i<n;i++)
+            {
+                if(n%i == 0)
+                {
+                    System.out.println("False");
+                    return;
+                }
+            }
+
+            System.out.println("True");
+        }
+    }
+}
